1c45c1789ae21f95aca30b2b3459f067288bc861
Henrik Mjaaland <noggerfog@hotmail.com>
2020-10-12 22:26:26 +0200

    Update RESTAPI.py

--- RESTAPI.py
+++ RESTAPI.py
@@ -2,23 +2,44 @@
 
 import psycopg2
 
-conn = psycopg2.connect("dbname=UserDB user=postgres password=test")
+conn = psycopg2.connect("dbname=UserDB user=postgres password=Ageofconan3")
 
 #port = 5432
 
 cur = conn.cursor()
 
-#Hent spesifikk bruker:
-#cur.execute('select <id> from "Users"')
-
 #Opprett bruker:
-cur.execute("""INSERT INTO "Users" (id, brukernavn, epost, passord) VALUES (3, 'x', 'y', 'z')""")
+def createUser():
+    cur.execute("""INSERT INTO "Users" (id, brukernavn, epost, passord) VALUES (3, 'x', 'y', 'z')""")
+
+#Hent spesifikk bruker:
+def getUser(userID):
+    cur.execute('select * from "Users" where id = %s',(str(userID)))
+
+createUser()
+getUser(3)
+
+rows = cur.fetchall()
+
+for r in rows:
+    print(r)
+
+
+
+
+
 
 #Slett spesifikk bruker:
-cur.execute('DELETE FROM "Users" WHERE id = 3')
+def delUser(userID):
+    cur.execute('DELETE FROM "Users" WHERE id = %s',(str(userID)))
 
 #Hent alle brukere:
-cur.execute('select * from "Users"')
+def getAll():
+    cur.execute('select * from "Users"')
+
+delUser(3)
+getAll()
+
 rows = cur.fetchall()
 
 for r in rows:
