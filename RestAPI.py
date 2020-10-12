a6c0a224f4a7a848a8d3e0b834f7961c35ec2a95
Henrik Mjaaland <noggerfog@hotmail.com>
2020-10-13 01:22:13 +0200

    Update RestAPI.py

--- RestAPI.py
+++ RestAPI.py
@@ -3,6 +3,12 @@
 !pip install flask_restful
 
 import psycopg2
+#import requests
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
 
 #Opprett bruker:
 def createUser(cur):
@@ -20,27 +26,55 @@ def delUser(cur, userID):
 def getAll(cur):
     cur.execute('select * from "Users"')
 
-def printRows(rows):
-    for r in rows:
-        print(r)
+#def printRows(rows):
+#    for r in rows:
+#        print(r)
 
 
 def main():
-    conn = psycopg2.connect("dbname=UserDB user=postgres password=test")
+    conn = psycopg2.connect("dbname=UserDB user=postgres password=Ageofconan3")
 
     #port = 5432
 
     cur = conn.cursor()
     createUser(cur)
-    getUser(cur,3)
-    rows = cur.fetchall()
-    printRows(rows)
+    #getUser(cur,3)
+    #rows = cur.fetchall()
+    #printRows(rows)
 
 
     delUser(cur,3)
     getAll(cur)
     rows = cur.fetchall()
-    printRows(rows)
+    #printRows(rows)
+    class CreateUser(Resource):
+        def post(self):
+            some_json = request.get_json()
+            return {'you sent': some_json}, 201
+
+    class DelUser(Resource):
+        def post(self):
+            some_json = request.get_json()
+            return {'you sent': some_json}, 201
+
+    class Users(Resource):
+        def get(self):
+            return rows
+
+    class UserID(Resource):
+        def get(self,num):
+            i = 0
+            for row in rows:
+                if row[0]==num:
+                    return row
+                else:
+                    if i==len(rows):
+                        return 'Non-existent user!'
+                i+=1
+
+    api.add_resource(Users, '/users')
+    api.add_resource(UserID, '/users/<int:num>')
+    app.run(debug=False)
 
     cur.close()
 
